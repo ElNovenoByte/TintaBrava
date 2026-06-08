9cf05209f1ad9b161e11106ad637013307b007da
JesusCaballeroBasilio <jesus.caballerobasilio@outlook.com>
2026-06-08 16:33:53 -0600

    Se añaden metodos de user

--- Backend/tintabrava/src/main/java/org/novenobyte/tintabrava/controller/UsuarioController.java
+++ Backend/tintabrava/src/main/java/org/novenobyte/tintabrava/controller/UsuarioController.java
@@ -1,10 +1,15 @@
 package org.novenobyte.tintabrava.controller;
 
-import org.novenobyte.tintabrava.service.ProductoService;
+import org.novenobyte.tintabrava.exceptions.ProductoNotFound;
+import org.novenobyte.tintabrava.model.Producto;
+import org.novenobyte.tintabrava.model.User;
 import org.novenobyte.tintabrava.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/usuarios") // Definir un endpoint de Class (general)
@@ -15,4 +20,40 @@ public class UsuarioController {
     public UsuarioController(UsuarioService usuarioService) {
         this.usuarioService = usuarioService;
     }
+
+    //Get
+    @GetMapping("/get/todos/usuarios")
+    public List<User> allUsuarios(){
+        return usuarioService.getUsers();
+    }
+
+    //Post
+    @PostMapping("/post/create")
+    public ResponseEntity<User> createUser(@RequestBody User newUser){
+
+        User createdUser = usuarioService.createUser(newUser);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(createdUser);
+    }
+    /*
+    //Post
+    @PostMapping("/post/nuevo-producto")
+    public ResponseEntity<Producto> createProducto(@RequestBody Producto newProducto){
+        Producto productoBySku = productoService.findProductoBySku(newProducto.getSku());
+
+        //Validar si el producto existe por sku
+        if(productoBySku != null){
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        } else {
+            //Si un producto no existe, lanzar un estatus: 201 CREATE
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .body(productoService.createProducto(newProducto));
+        }
+    }
+
+
+     */
+
+
 }
